6293a6411d1ac2ac7bd1e8d535728db3b394e4ba
Md Rahad Hossen <mdrahadhossen811@gmail.com>
2025-05-27 16:01:34 +0600

    Update index.js
    bug fix

--- index.js
+++ index.js
@@ -104,7 +104,9 @@ app.post("/download", async (req, res) => {
       output: path.join(downloadsDir, "%(title)s.%(ext)s"),
       format: "bestvideo+bestaudio/best",
       mergeOutputFormat: "mp4",
-      cookies: "cookies.txt"
+      cookies: "cookies.txt",
+      cookiesFromBrowser: "firefox",
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     });
 
     const files = fs.readdirSync(downloadsDir);
